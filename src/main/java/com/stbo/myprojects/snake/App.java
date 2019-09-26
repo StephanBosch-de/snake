0e3216e851a1a685c1fc2fef290eb60f0b3b1624
Stephan <stephanbosch@gmx.de>
2019-09-26 14:15:24 +0200

    Set up new Maven Project in Eclipse

---
+++ src/main/java/com/stbo/myprojects/snake/App.java
@@ -0,0 +1,13 @@
+package com.stbo.myprojects.snake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
